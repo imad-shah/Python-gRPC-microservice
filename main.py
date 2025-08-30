ba09dffb327d8deb947279b22a4f4ad31ea57049
imad-shah <ishah5@student.gsu.edu>
2025-08-30 16:03:41 -0400

    added more methods to be implemented

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from proto.library_pb2_grpc import LibraryServicer
 
 class LibraryServicer(LibraryServicer):
     # Fetch name, return books
-    def GetBooks(name:str):
+    def GetBooks():
         ...
-
 
     def GetInventorySummary():
         ...
+
     def GetBookCount():
+        ...
+
+
+    def CheckoutBook():
+        ...
+
+
+    def ReturnBook():
         ...
