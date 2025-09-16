f766043cd86a62e8a5bf92c2bfdebb790ee197ca
imad-shah <ishah5@student.gsu.edu>
2025-09-16 03:28:05 -0400

    [models] add API data model

---
+++ models/book.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+@dataclass
+class Book:
+    author: str
+    title: str
+    isbn: str
+    copies_remaining: int
+    total_copies: int
+
+    def get_tuple(self):
+        return self.author, self.title, self.isbn, self.copies_remaining, self.total_copies 
