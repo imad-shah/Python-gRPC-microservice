5b955bb113155568a667c67acb3bcf617cb99f71
imad-shah <ishah5@student.gsu.edu>
2025-09-30 03:35:42 -0400

    [repository] implemented database access logic

--- repository/book_repository.py
+++ repository/book_repository.py
@@ -12,7 +12,11 @@ class IBookRepository(ABC):
 
 
     @abstractmethod
-    def GetBooks(self, id : int) -> list[str]:
+    def GetBook(self, isbn: int) -> Book | None:
+        pass
+
+    @abstractmethod
+    def GetBooksFromPatron(self, id : int) -> list[int]:
         pass
 
     @abstractmethod
@@ -20,19 +24,39 @@ class IBookRepository(ABC):
         pass
 
     @abstractmethod
-    def GetBookCount(self, isbn: str) -> int:
+    def GetBookCount(self, isbn: int) -> int:
+        pass
+    
+    @abstractmethod
+    def GetTotalCopies(self, isbn: int) -> int:
         pass
 
     @abstractmethod
-    def IncrementCopies(self, isbn : str) -> None:
+    def IncrementCopies(self, isbn : int) -> None:
         pass
 
     @abstractmethod
-    def DecrementCopies(self, isbn : str) -> None:
+    def DecrementCopies(self, isbn : int) -> None:
+        pass
+    
+    @abstractmethod
+    def AddBookToPatron(self, patron : int, isbn : int) -> None:
+        pass
+
+    @abstractmethod
+    def RemoveBookFromPatron(self, patron : int, isbn: int) -> None:
         pass
 
     @abstractmethod
     def CreatePatron(self, patron: str):
+        pass
+
+    @abstractmethod
+    def GetBookById(self, isbn : int) -> Book:
+        pass
+   
+    @abstractmethod
+    def DeleteBookByIsbn(self, isbn : int) -> bool:
         pass
 
 class BookRepository(IBookRepository):
@@ -40,7 +64,16 @@ class BookRepository(IBookRepository):
         self._sqldb = sqldb
         self._mongodb = mongodb
 
-    def GetBooks(self, id: int) -> list[str]:
+
+    def GetBook(self, isbn: int) -> Book | None:
+        cursor = self._sqldb.cursor()
+        cursor.execute(GET_QUERY, (isbn,))
+        row = cursor.fetch_one()
+        if not row:
+            return None
+        return Book(*row)
+
+    def GetBooksFromPatron(self, id: int) -> list[int]:
         doc = self._mongodb.find({'id' : id})
         if doc:
             books = doc['books']
@@ -58,28 +91,56 @@ class BookRepository(IBookRepository):
         rows = cursor.fetchall()
         return [Book(isbn, title, author, copies_remaining, total_copies) for isbn, title, author, copies_remaining, total_copies in rows]
 
-    def GetBookCount(self, isbn: str) -> int:
+    def GetBookCount(self, isbn: int) -> int:
         cursor = self._sqldb.cursor(dictionary = True)
         cursor.execute(GET_QUERY, (isbn,))
         row = cursor.fetch_one()
         return row['copies_remaining']
 
-    def IncrementCopies(self, isbn : str) -> None:
+    def GetTotalCopies(self, isbn: int) -> int:
+        cursor = self._sqldb.cursor(dictionary = True)
+        cursor.execute(GET_QUERY, (isbn,))
+        row = cursor.fetch_one()
+        return row['total_copies']
+
+    def IncrementCopies(self, isbn : int) -> None:
         cursor = self._sqldb.cursor()
         cursor.execute('UPDATE Books SET copies_remaining = copies_remaining + 1 WHERE isbn = %s', (isbn,))
         self._sqldb.commit()
         return None
 
-    def DecrementCopies(self, isbn : str) -> None:
+    def DecrementCopies(self, isbn : int) -> None:
         cursor = self._sqldb.cursor()
         cursor.execute('UPDATE Books SET copies_remaining = copies_remaining - 1 WHERE isbn = %s', (isbn,))
         self._sqldb.commit()
         return None
-
+    
+    def AddBookToPatron(self, patron: int, isbn: int) -> None:
+        self._mongodb.update_one(
+                {"id": patron},
+                {"$push": {"books": isbn}}
+                )
+    
+    def RemoveBookFromPatron(self, patron: int, isbn: int) -> None:
+        self._mongodb.update_one(
+                {"id": patron},
+                {"$pull": {"books": isbn}}
+                )
 
     def CreatePatron(self, patron: str):
         last_id = self._mongodb.find_one().sort({"ID"}, -1)
         insert = {'id' : last_id + 1, 'name' : patron, 'books' : []}
         self._mongodb.insert_one(insert)
+    
+    def GetBookById(self, isbn: int) -> Book:
+        cursor = self._sqldb.cursor()
+        cursor.execute(GET_QUERY, (isbn,))
+        row = cursor.fetch_one()
+        return Book(*row)
 
+    def DeleteBookByIsbn(self, isbn: int) -> bool:
+        cursor = self._sqldb.cursor()
+        cursor.execute(DELETE_QUERY, (isbn,))
+        self._sqldb.commit()
+        return True
 
